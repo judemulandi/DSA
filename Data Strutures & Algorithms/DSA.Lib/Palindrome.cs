cdf4d8d2d9ce924e2a06e2d02ffd0a2adb40ab1d
jude <jude.mulandi@gmail.com>
2024-02-19 06:14:09 +0300

    add palindrome

---
+++ Data Strutures & Algorithms/DSA.Lib/Palindrome.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DSA.Lib
+{
+    public static class Palindrome
+    {
+        public static bool IsPalindrome(string str)
+        {
+            CustomStack stack = new CustomStack();
+            int pos = 0;
+            Boolean isPalindrome = false;
+            string ch;
+            if (str == null) return isPalindrome;
+
+            for (int i = 0; i < str.Length; i++)
+                stack.Push(str.Substring(i,1));
+
+            while(stack.Count > 0)
+            {
+                ch = stack.Pop().ToString();
+                if (ch != str.Substring(pos, 1))
+                {
+                    isPalindrome = false;
+                    break;
+                }
+                else
+                    isPalindrome = true;
+                pos++;
+            }
+
+            return isPalindrome;
+        }
+    }
+}
--- Data Strutures & Algorithms/DSA.Lib/obj/Debug/net8.0/DSA.Lib.AssemblyInfo.cs
+++ Data Strutures & Algorithms/DSA.Lib/obj/Debug/net8.0/DSA.Lib.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DSA.Lib")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c4a4ad8055aa4316c344f7fb47ee1ea21bd0c04d")]
 [assembly: System.Reflection.AssemblyProductAttribute("DSA.Lib")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DSA.Lib")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
